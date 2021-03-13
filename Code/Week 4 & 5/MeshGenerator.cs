ea315b28686899ed9b11e3d7a70fc079f7f10e90
nielsgraber <78588853+nielsgraber@users.noreply.github.com>
2021-03-13 01:15:37 +0100

    Add files via upload

--- Code/Week 4 & 5/MeshGenerator.cs
+++ Code/Week 4 & 5/MeshGenerator.cs
@@ -11,6 +11,9 @@ public class MeshGenerator : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    public int xSize = 20;
+    public int zSize = 20; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,20 +27,41 @@ public class MeshGenerator : MonoBehaviour
 
 void CreateShape()
 {
-    vertices = new Vector3[]
-    {
-        new Vector3 (0, 0, 0),
-        new Vector3 (0, 0, 1),
-        new Vector3 (1, 0, 0),
-        new Vector3 (1, 0, 1)
-    };
+    vertices = new Vector3[(xSize + 1) * (zSize + 1)];
 
-    triangles = new int[]
-    {
-        0, 1, 2,
-        1, 3, 2
-    };
 
+    for (int i = 0, z = 0; z <= zSize; z++)
+    {
+        for (int x = 0; x <= xSize; x++)
+        {
+            float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+            vertices[i] = new Vector3(x, y, z);
+            i++;
+        }
+
+    }
+
+    triangles = new int[xSize * zSize * 6];
+
+    int vert = 0;
+    int tris = 0;
+
+    for (int z = 0; z < zSize; z++)
+    {
+        for(int x = 0; x < xSize; x++ )
+        {
+            triangles[tris + 0] = vert + 0;
+            triangles[tris + 1] = vert + xSize + 1;
+            triangles[tris + 2] = vert + 1;
+            triangles[tris + 3] = vert + 1;
+            triangles[tris + 4] = vert + xSize + 1;
+            triangles[tris + 5] = vert + xSize + 2;
+
+            vert++;
+            tris += 6;
+        }
+        vert++;
+    }
 }
 
 void UpdateMesh()
